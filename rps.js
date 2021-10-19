3cede31fb5ab30feeae61d435ab49b1b3fec42a6
Bach Vu <vugiabachde@gmail.com>
2021-10-19 11:14:20 +0200

    added UI's and a 'Game over' mechanic (BO5)

--- rps.js
+++ rps.js
@@ -1,3 +1,4 @@
+const mainBody = document.querySelector('body');
 function capitalizeFirstLetter(string) {
     return string[0].toUpperCase() + string.slice(1,string.length)  
 }
@@ -5,14 +6,10 @@ function computerPlay() {
     const hand = ["rock","paper","scissor"]
     return hand[Math.floor(Math.random()*hand.length)]
 }
-function playRound() {
-    let state = 0
+function playRound(e) {
+    const playerSelection = this.id;
+    let state = 0;
     const computerSelection = computerPlay()
-    let playerSelection = ''
-    playerSelection = prompt("rock, paper, scissor!").toLowerCase()
-    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissor") {
-        playerSelection = prompt("You must choose either rock, paper or scissor!").toLowerCase()
-    }
     if (computerSelection !== playerSelection) {
         if (playerSelection === "rock") {
             if (computerSelection === "scissor") {
@@ -30,15 +27,74 @@ function playRound() {
             } else { state = -1}
         }
     }
+    console.log(state,playerSelection,computerSelection)
+    const result = document.createElement('p');
+    result.style.marginTop = "30px";
+    result.setAttribute("id","result")
+  
+    if (document.contains(document.getElementById("result"))) {
+        document.getElementById("result").remove();
+    }
+    if (!document.contains(document.getElementById("scores"))) {
+        const scores = document.createElement('div');
+        scores.setAttribute("id","scores");
+        const playerScore = document.createElement("p");
+        playerScore.setAttribute("id","player-score");
+        playerScore.textContent = 0;
+        const computerScore = document.createElement("p");
+        computerScore.setAttribute("id","computer-score");
+        computerScore.textContent = 0;
+        const colon = document.createElement("p");
+        colon.textContent = ' : '
+        scores.appendChild(playerScore);
+        scores.appendChild(colon);
+        scores.appendChild(computerScore);
+        scores.style.display = "flex";
+        scores.style.flexDirection = "row";
+        scores.style.marginTop = "30px";
+        mainBody.appendChild(scores)
+
+    }
+    if (document.contains(document.getElementById("announcement"))) {
+        document.getElementById("announcement").remove();
+        document.getElementById("player-score").innerText = 0;
+        document.getElementById("computer-score").innerText = 0;
+    }
     switch (state) {
         case 1:
-            return (`You won! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`)
+            result.textContent = `You won! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`
+            document.getElementById("player-score").innerText = Number(document.getElementById("player-score").textContent) + 1
+            mainBody.appendChild(result)
+            //return (`You won! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`)
+            break;
         case -1:
-            return (`You lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`)
+            result.textContent = `You lost! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`
+            document.getElementById("computer-score").innerText = Number(document.getElementById("computer-score").textContent) + 1
+            mainBody.appendChild(result)
+            //return (`You lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`)
+            break;
         default:
-            return (`It's a draw! You both chose ${capitalizeFirstLetter(playerSelection)   }`)
+            result.textContent = `It's a draw! You both chose ${capitalizeFirstLetter(playerSelection)}`
+            mainBody.appendChild(result)
+            //return (`It's a draw! You both chose ${capitalizeFirstLetter(playerSelection)}`)
+    }
+    const computerOverallScore = document.getElementById("computer-score").textContent;
+    const playerOverallScore = document.getElementById("player-score").textContent;
+    if (computerOverallScore === "5"||playerOverallScore === "5") {
+        const [announcementContent, textColor] = (playerOverallScore === "5") ? ["Game over! You have defeated the computer","#4EE2A0"]: ["Game over! The computer has defeated you!","#FC6666"];
+        const announcement = document.createElement("p");
+        announcement.setAttribute("id","announcement")
+        announcement.textContent = announcementContent;
+        announcement.style.cssText = "marginTop : 20px";
+        announcement.style.color = textColor; 
+        mainBody.appendChild(announcement);
+
     }
 }
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener("click", playRound))
+
+
 /*function game() {
     let counter = 0
     let playerScore = 0
